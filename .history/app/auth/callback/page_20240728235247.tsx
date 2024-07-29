f8f54c369114e7ebbf2d748cb82d01c4a954b8ba
Ehsan Alborzi <ehsanalborzi2009@gmail.com>
2024-07-29 16:10:47 +0200

    created webhook folder and auth folder

---
+++ .history/app/auth/callback/page_20240728235247.tsx
@@ -0,0 +1,10 @@
+import { Loader2 } from 'lucide-react';
+
+const Page = () => {
+  return (
+    <div className="flex justify-center items-center">
+      <Loader2 className="animate-spin" />
+    </div>
+  );
+};
+export default Page;
